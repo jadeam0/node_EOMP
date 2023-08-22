c152c8d548a3e1a933a7ce1f1ee3e73475de0ae9
jadeam0 <jdpaulse27@gmail.com>
2023-08-22 12:31:31 +0200

    static files complete

--- api/index.js
+++ api/index.js
@@ -8,8 +8,9 @@ const { errorHandling } = require('./middleware/errorHandeling');
 const { routes } = require('./controller/index')
 const Port = process.env.PORT || 4000;
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.static('./static'));
 
+app.use(express.urlencoded({extended: false}));
 
 app.use(
   cors(),
@@ -17,10 +18,11 @@ app.use(
   express.json(),
   routes
 );
-
-
-
 app.use(errorHandling);
+
+routes.get('^/$|/node_EOMP', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './static/html/index.html'));
+});
 
 app.listen(Port, () => {
   console.log(`The server is running on port ${Port}`);
